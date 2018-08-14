17dd7fbe809ad359afcb043c28d8aa996de5ba56
tknr <info@tknr.com>
2018-08-14 17:47:59 +0900

    2018年  8月 14日 火曜日 17:47:59 JST

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+mkdir -p ~/bin
+ln -s `pwd`/google ~/bin/
+ln -s `pwd`/urlencode ~/bin/
+ln -s `pwd`/urldecode ~/bin/
+
+export_count=`cat ~/.bash_profile | grep 'export PATH="$HOME/bin:$PATH"' | wc -l`
+echo $export_count
+
+if [ $export_count -eq 0 ]; then
+	echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bash_profile
+fi
+echo "installed."
+echo "execute to complete installation:"
+echo "source ~/.bash_profile"
